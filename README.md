69e80597e0898ff31b7ddf01143f446ae9006fa4
ApacheThunder <spenserwhaley@gmail.com>
2024-10-28 17:29:44 -0500

    Sync to recent changes from original repo.
    * Lost some of the extra changes I did but I don't think they were that
    important anyways. Now it's mainly the original version but with new
    font and better screen init.
    * BlocksDS make file remains unchanged. I do not have this build system
    setup right now so I can't attempt adding the custom font graphiocs to
    it.
    * First palette entry for new icon.bmp changed from pink to black. My
    build system seems to produce an icon with pink background due to this.
    This is no buano so I had to correct this. I'm not sure how asie is
    getting the correct result from this. Since the icon doesn't use pink
    anywhere anyways this was a minimal change so shouldn't change the look
    of the icon any.
    * Added a gitignore. Mostly to prevent dldi files I currently have in my
    project folder from showing up on the repo.
    * Padding script updated to match new one. Looks like this app uses a
    8mb pad file instead of 16MB.

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# nds-sd-benchmark
+# dldi-driver-benchmark
 
 Simple filesystem I/O benchmark for NDS homebrew.
+
+The benchmark expects an 8 MB file "benchmark_pad.bin" in the root of the CF/SD card.
+This file will be created automatically if it doesn't exist, but you might want to create it manually
+for read-only storage mediums.
 
 # Changes from original build
 
 * Compiled with nds329 for 32KB DLDI support.
-* Better font for console text.
+* My build system doesn't like make file having dkp so currently there is a makefile with no extension provided. It is currently identical to the dkp file right now.
+* Better font for console text. Associeted changes to make file to make this work. May need further tweeks if one wishes to use the "make -f Makefile.dkp" command.
 * Top screen initialized so it doesn't remain white.
-* nitroFS as source for pad.bin temporarily disabled. pad.bin is expected to be on root of flashcart's file system now. This is due to bug in write code that ends up overwriting the NDS file!
 * BlocksDS makefile hasn't been updated to support the new font file. Someone else should look into this as I'm unfamiliar with that make system right now.
-* Random Write function appears to be broken. (hangs on first 0.5MiB write test). This bug existed in original build...I do not know why it's happening right now so remains unfixed.
-* ITCM_CODE flag used for read/write functions...may improve performance a bit?
 
 ## Building
 
-    $ ./generate_padding.sh
     $ make -f Makefile.blocks # or Makefile.dkp
 
 ## License
--- source/main.c
+++ source/main.c
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: MIT
 //
-// SPDX-FileContributor: Adrian "asie" Siekierka, 2023
+// SPDX-FileContributor: Adrian "asie" Siekierka, 2023, 2024
 
 #include <stdio.h>
 
 #include <fat.h>
 #include <nds.h>
+#include <unistd.h>
 
 #include <nds/arm9/dldi.h>
 
 #include "font.h"
 
-const char *nds_filename;
+const char *pad_filename = "/benchmark_pad.bin";
+#define PAD_FILE_SIZE (8*1024*1024)
 uint8_t *io_buffer;
 int io_buffer_size;
 int io_read_offset = 0;
 bool lookup_cache_enabled = true;
+bool fat_initialized = false;
+
+static char options[20][33];
 
 static PrintConsole tpConsole;
 static PrintConsole btConsole;
@@ -23,12 +28,9 @@ static PrintConsole btConsole;
 static int bg;
 static int bgSub;
 
-
 static inline uint32_t my_rand(void) {
     static uint32_t seed = 0;
-
     seed = seed * 0xFDB97531 + 0x2468ACE;
-
     return seed;
 }
 
@@ -36,10 +38,54 @@ static inline uint32_t get_ticks(void) {
     return TIMER0_DATA | (TIMER1_DATA << 16);
 }
 
-ITCM_CODE static void benchmark_read(bool sequential) {
-    FILE *file = fopen(nds_filename, "rb");
+static void create_pad_file(void) {
+    FILE *file;
+    uint32_t buffer[256];
+
+    file = fopen(pad_filename, "rb");
+    if (file != NULL) {
+        fclose(file);
+        return;
+    }
+
+    file = fopen(pad_filename, "wb");
     if (file == NULL) {
-        printf("\x1b[41mCould not open '%s'!\n", nds_filename);
+        printf("\x1b[41mCould not create pad file!\n");
+        return;
+    }
+    printf("Creating pad file... ");
+    for (int i = 0; i < PAD_FILE_SIZE; i += sizeof(buffer)) {
+        for (int k = 0; k < sizeof(buffer) / 4; k++) {
+            buffer[k] = my_rand();
+        }
+        if (fwrite(buffer, sizeof(buffer), 1, file) <= 0) {
+            fclose(file);
+            unlink(pad_filename);
+            printf("\x1b[41mError!\n");
+            return;
+        }
+    }
+    printf("OK\n");
+    fclose(file);
+}
+
+static void fat_init(void) {
+    if (!fat_initialized) {
+        if (!fatInitDefault()) {
+            printf("\x1b[41mFAT init failed!\n");
+            // Set fat_initialized anyway; the file open operations
+            // will fail instead.
+        }
+        create_pad_file();
+        fat_initialized = true;
+    }
+}
+
+static void benchmark_read(bool sequential) {
+    fat_init();
+    FILE *file = fopen(pad_filename, "rb");
+    if (file == NULL) {
+        printf("\x1b[41mCould not open '%s'!\n", pad_filename);
         return;
     }
 //    printf("%d\n",
@@ -67,15 +113,17 @@ ITCM_CODE static void benchmark_read(bool sequential) {
             int pos = 0;
 	    while (reads < reads_count) {
 		if (pos == 0)
-	                fseek(file, io_read_offset, SEEK_SET);
-                fread(io_buffer, curr_size, 1, file);
-		pos = (pos + curr_size) & 0x7FFFFF;
+	            fseek(file, io_read_offset, SEEK_SET);
+                if (fread(io_buffer, curr_size, 1, file) <= 0)
+                    break;
+		pos = (pos + curr_size) & 0x3FFFFF;
                 reads++;
             }
 	} else {
 	    while (reads < reads_count) {
-                fseek(file, ((my_rand() & (~0x1FF)) & 0x7FFFFF) + io_read_offset, SEEK_SET);
-                fread(io_buffer, curr_size, 1, file);
+                fseek(file, ((my_rand() & (~0x1FF)) & 0x3FFFFF) + io_read_offset, SEEK_SET);
+                if (fread(io_buffer, curr_size, 1, file) <= 0)
+                    break;
                 reads++;
             }
 	}
@@ -84,7 +132,9 @@ ITCM_CODE static void benchmark_read(bool sequential) {
 	    uint32_t ticks_diff = ticks_end - ticks_start;
         double seconds_time = (ticks_diff) / (double)(BUS_CLOCK >> 8);
         double kilobytes_per_second = read_kilobytes / seconds_time;
-        if (kilobytes_per_second >= 1024.0) {
+        if (reads < reads_count) {
+            sprintf(msg_buffer, "\x1b[41mERROR");
+        } else if (kilobytes_per_second >= 1024.0) {
             sprintf(msg_buffer, "%.3f MB/s", kilobytes_per_second / 1024.0);
         } else {
             sprintf(msg_buffer, "%.3f KB/s", kilobytes_per_second);
@@ -97,11 +147,11 @@ ITCM_CODE static void benchmark_read(bool sequential) {
     fclose(file);
 }
 
-ITCM_CODE static void benchmark_write(bool sequential) {
-    FILE *file = fopen(nds_filename, "r+b");
-    int file_offset = 8*1024*1024;
+static void benchmark_write(bool sequential) {
+    fat_init();
+    FILE *file = fopen(pad_filename, "r+b");
     if (file == NULL) {
-        printf("\x1b[41mCould not open '%s'!\n", nds_filename);
+        printf("\x1b[41mCould not open '%s'!\n", pad_filename);
         return;
     }
 #ifdef BLOCKSDS
@@ -128,16 +178,19 @@ ITCM_CODE static void benchmark_write(bool sequential) {
         uint32_t reads = 0;
         if (sequential) {
             int pos = 0;
-			while (reads < reads_count) {
-			if (pos == 0)fseek(file, io_read_offset, SEEK_SET);
-                fwrite(io_buffer, curr_size, 1, file);
-				pos = (pos + curr_size) & 0x1FFFFF;
+	    while (reads < reads_count) {
+		if (pos == 0)
+	            fseek(file, io_read_offset, SEEK_SET);
+                if (fwrite(io_buffer, curr_size, 1, file) <= 0)
+                    break;
+		pos = (pos + curr_size) & 0x1FFFFF;
                 reads++;
             }
         } else {
             while (reads < reads_count) {
-                fseek(file, file_offset + ((my_rand() & (~0x1FF)) & 0x1FFFFF) + io_read_offset, SEEK_SET);
-                fwrite(io_buffer, curr_size, 1, file);
+                fseek(file, ((my_rand() & (~0x1FF)) & 0x1FFFFF) + io_read_offset, SEEK_SET);
+                if (fwrite(io_buffer, curr_size, 1, file) <= 0)
+                    break;
                 reads++;
             }
         }
@@ -146,7 +199,9 @@ ITCM_CODE static void benchmark_write(bool sequential) {
 	    uint32_t ticks_diff = ticks_end - ticks_start;
         double seconds_time = (ticks_diff) / (double)(BUS_CLOCK >> 8);
         double kilobytes_per_second = read_kilobytes / seconds_time;
-        if (kilobytes_per_second >= 1024.0) {
+        if (reads < reads_count) {
+            sprintf(msg_buffer, "\x1b[41mERROR");
+        } else if (kilobytes_per_second >= 1024.0) {
             sprintf(msg_buffer, "%.3f MB/s", kilobytes_per_second / 1024.0);
         } else {
             sprintf(msg_buffer, "%.3f KB/s", kilobytes_per_second);
@@ -159,8 +214,6 @@ ITCM_CODE static void benchmark_write(bool sequential) {
     fclose(file);
 }
 
-char options[20][33];
-
 static bool run_menu(int options_count, int *selection) {
     int last_selection = -1;
     int max_option_width = 0;
@@ -172,7 +225,7 @@ static bool run_menu(int options_count, int *selection) {
 
     while (1) {
         if (last_selection != *selection) {
-            consoleClear();
+            printf("\x1b[2J"); // Clear console
             for (int i = 0; i < options_count; i++) {
                 printf("\x1b[%dC\x1b[46m%c\x1b[39m %s\n", menu_left, i == *selection ? '>' : ' ', options[i]);
             }
@@ -205,7 +258,8 @@ static void press_start_to_continue(void) {
     }
 }
 
-void CustomConsoleInit() {
+
+static void CustomConsoleInit() {
 	videoSetMode(MODE_0_2D);
 	videoSetModeSub(MODE_0_2D);
 	vramSetBankA (VRAM_A_MAIN_BG);
@@ -233,17 +287,21 @@ void CustomConsoleInit() {
 
 int main(int argc, char **argv) {
     CustomConsoleInit();
-
     consoleClear();
-	
-	//      01234567890123456789012345678901
-    printf("  \x1b[43m*\x1b[39m SD file system benchmark \x1b[43m*\x1b[39m\n");
-    printf("\x1b[37mDLDI: %s\x1b[39m\n\n", io_dldi_data->friendlyName);
+    // printf("\x1b[2J"); // Clear console
 
-    // set window
-    // consoleSetWindow(NULL, 0, 3, 32, 25);
-	consoleSetWindow(&btConsole, 0, 3, 32, 25);
+    //      01234567890123456789012345678901
+    printf(" \x1b[43m*\x1b[39m DLDI driver benchmark v0.2 \x1b[43m*\x1b[39m\n");
+    printf("\x1b[37m%s\x1b[39m\n\n", io_dldi_data->friendlyName);
+
+    // set windows
+    consoleSetWindow(&btConsole, 0, 3, 32, 25);
 	consoleSetWindow(&tpConsole, 0, 3, 32, 25);
+
+    if (argc <= 0 || argv[0][0] == 0) {
+        printf("\x1b[41mCould not find argv!\n");
+        goto exit;
+    }
 
     io_buffer_size = 2*1024*1024;
     io_buffer = malloc(io_buffer_size);
@@ -252,33 +310,14 @@ int main(int argc, char **argv) {
         goto exit;
     }
 
-    if (!fatInitDefault()) {
-		printf("\x1b[41mFAT Init Failed!\n");
-		goto exit;
-	}
-	
-    /*if (argc <= 0 || argv[0][0] == 0) {
-		// arg not passed. Try and use external file instead.
-        nds_filename = "/pad.bin";
-		// printf("\x1b[41mCould not find argv!\n");
-        // goto exit;
-    } else {
-		nds_filename = argv[0];
-	}*/
-	
-	// NitroFS support removed until nds file overwrite bug is fixed
-	nds_filename = "/pad.bin";
-	
-	if (access(nds_filename, F_OK) != 0) {	
-		printf("\x1b[41mCould not find pad.bin!\n");
-		goto exit;
-	}
-	
-
     TIMER0_DATA = 0;
     TIMER1_DATA = 0;
     TIMER0_CR = TIMER_ENABLE | TIMER_DIV_256;
     TIMER1_CR = TIMER_ENABLE | TIMER_CASCADE;
+
+#ifdef BLOCKSDS
+    dldiSetMode(DLDI_MODE_ARM9);
+#endif
 
     int options_count;
     int selection = -1;
@@ -295,7 +334,10 @@ int main(int argc, char **argv) {
                 else if (io_read_offset >= 256) io_read_offset = 0;
                 else io_read_offset <<= 1;
                 break;
+#ifdef BLOCKSDS
             case 6: lookup_cache_enabled = !lookup_cache_enabled; break;
+            case 7: if (!fat_initialized) dldiSetMode(dldiGetMode() == DLDI_MODE_ARM7 ? DLDI_MODE_ARM9 : DLDI_MODE_ARM7); break;
+#endif
         }
 
         snprintf(options[0], 33, "Bench. random reads");
@@ -306,7 +348,8 @@ int main(int argc, char **argv) {
         snprintf(options[5], 33, "Byte offset: %d", io_read_offset);
 #ifdef BLOCKSDS
         snprintf(options[6], 33, "Seek lookup cache: %s", lookup_cache_enabled ? "Yes" : "No");
-        options_count = 7;
+        snprintf(options[7], 33, "DLDI CPU: %s", dldiGetMode() == DLDI_MODE_ARM7 ? "ARM7" : "ARM9");
+        options_count = 8;
 #else
         options_count = 6;
 #endif
